401f163b9467c1b461ef99e5b9da155e099e66a4
Toby Johnston <toby@crssoftware.co.nz>
2018-08-28 11:04:15 +1200

    basic message list component

---
+++ components/MessageList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MessageList extends React.component {
+    render () {
+        return (
+            <div className="message-list">
+                <div className="help-text">MessageList</div>
+            </div>
+        )
+    }
+}
+
+export default MessageList
