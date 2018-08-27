c47d2dfc89df098769890204bfe554f69fe4640a
Toby Johnston <toby@crssoftware.co.nz>
2018-08-28 11:44:19 +1200

    updated messageList

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    }
+]
+
 class MessageList extends React.component {
     render () {
         return (
             <div className="message-list">
-                <div className="help-text">MessageList</div>
+                {DUMMY_DATA/Map((message, index) => {
+                    return (
+                        <div key={index} className="message">
+                            <div className="message-username">{message.senderId}</div>
+                            <div className="message-text">{message.text}</div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
