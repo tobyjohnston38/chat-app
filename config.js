2ba8873dd8acb52932b9b78008e83da32eeada4f
Toby Johnston <toby@crssoftware.co.nz>
2018-08-28 16:15:27 +1200

    added chatkit

---
+++ config.js
@@ -0,0 +1,5 @@
+const tokenUrl = 
+"https://us1.pusherplatform.io/services/chatkit_token_provider/v1/024f404f-a4e2-49da-9ca0-e92d1a278897/token"
+const instanceLocator = "v1:us1:024f404f-a4e2-49da-9ca0-e92d1a278897";
+
+export { tokenUrl, instanceLocator }
--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React from 'react'
+import Chatkit from '@pusher/chatkit'
 import MessageList from '../src/components/MessageList'
-// import SendMessage from '../src/components/SendMessageForm'
-// import RoomList from '../src/components/RoomList'
-// import NewRoomForm from '../src/components/NewRoomForm'
+import SendMessage from '../src/components/SendMessageForm'
+import RoomList from '../src/components/RoomList'
+import NewRoomForm from '../src/components/NewRoomForm'
+
+import { tokenUrl, instanceLocator } from './config'
 
 class App extends React.Component {
+
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator,
+            userId: 'Toby',
+            tokenProvider: newChatkit.tokenProvider({
+                url: tokenUrl
+            })
+        })
+
+        chatManager.connect()
+        .then(currentUser => {
+            currentUser.subscribeToRoom({
+                roomId: 14868724,
+                hooks: {
+                    onNewMessage: message => {
+                        console.log('message.text: ', message.text);
+                    }
+                }
+            })
+        })
+    }
+
     render() {
         return (
             <div className="app">
-                {/* <RoomList /> */}
+                <RoomList />
                 <MessageList />
-                {/* <SendMessage /> */}
-                {/* <NewRoomForm /> */}
+                <SendMessage />
+                <NewRoomForm />
             </div>
         );
     }
