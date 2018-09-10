4abe92b0d705bcb85f9a0945553e017b20d733b8
Toby Johnston <toby@crssoftware.co.nz>
2018-09-11 09:34:45 +1200

    autoscroll

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+import Message from './Message'
 
 class MessageList extends React.Component {
+    
+    componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this)
+        this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
+    
+    componentDidUpdate() {
+        if (this.shouldScrollToBottom) {
+            const node = ReactDOM.findDOMNode(this)
+            node.scrollTop = node.scrollHeight   
+        }
+    }
+    
     render() {
         return (
             <div className="message-list">
                 {this.props.messages.map((message, index) => {
                     return (
-                        <div key={index} className="message">
-                            <div className="message-username">{message.senderId}</div>
-                            <div className="message-text">{message.text}</div>
-                        </div>
+                        <Message key={message.id} username={message.senderId} text={message.text} />
                     )
                 })}
             </div>
